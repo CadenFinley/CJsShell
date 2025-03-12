2d5fcbf0edd5c0178e525a93f5a91fefe7781de0
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-03-12 03:37:43 -0500

    Update ClaudeAnthropic.cpp

--- example_plugins/ClaudeAnthropic.cpp
+++ example_plugins/ClaudeAnthropic.cpp
@@ -10,6 +10,8 @@
 #include <queue>
 #include "nlohmann/json.hpp"
 
+// clang++ -dynamiclib -fPIC -O2 -std=c++11 -o ClaudeAnthropic.dylib ClaudeAnthropic.cpp -lcurl
+
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
