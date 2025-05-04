44c231f743b6bce881456d0737e6e120b0224a50
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-05-04 17:08:10 -0500

    Update shell.cpp

--- src/shell.cpp
+++ src/shell.cpp
@@ -29,9 +29,9 @@ void shell_signal_handler(int signum, siginfo_t* info, void* context) {
     case SIGCHLD: {
       sigchld_received = 1;
       if (g_debug_mode) std::cerr << "DEBUG: SIGCHLD handler executed" << std::endl;
+      while (waitpid(-1, nullptr, WNOHANG) > 0) {}
       break;
     }
-    while (waitpid(-1, nullptr, WNOHANG) > 0) {}
       
     case SIGHUP: {
       sighup_received = 1;
