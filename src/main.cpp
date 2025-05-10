4d0258461b271833d881026b348879227c1b1f40
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-05-10 01:46:55 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,10 @@
  * 0       - Success
  * 1       - General errors/Catchall
  * 2       - Misuse of shell builtins or syntax error
- * 126     - Command invoked cannot execute (permission problem or not
- * executable) 127     - Command not found 128     - Invalid argument to exit
+ * 126     - Command invoked cannot execute (permission problem 
+ * or not executable) 
+ * 127     - Command not found 
+ * 128     - Invalid argument to exit
  * 128+n   - Fatal error signal "n" (e.g., 130 = 128 + SIGINT(2) = Control-C)
  * 130     - Script terminated by Control-C (SIGINT)
  * 137     - Process killed (SIGKILL)
