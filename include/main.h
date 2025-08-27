c37523cf0bedae87bd5adbe6625ee95efa084b46
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-08-27 13:38:19 -0500

    Update main.h

--- include/main.h
+++ include/main.h
@@ -15,7 +15,7 @@
 #include "theme.h"
 #include "update.h"
 
-const std::string c_version = "2.2.9";
+const std::string c_version = "2.3.0";
 const std::string c_github_url = "https://github.com/CadenFinley/CJsShell";
 const std::string c_update_url =
     "https://api.github.com/repos/cadenfinley/CJsShell/releases/latest";
