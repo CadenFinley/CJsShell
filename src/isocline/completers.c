1b664f17058366914c39e9890c2d1f6c83e9c1c2
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-09-01 03:28:36 -0500

    Update completers.c

--- src/isocline/completers.c
+++ src/isocline/completers.c
@@ -6,6 +6,7 @@
 -----------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 #include "isocline/isocline.h"
 #include "isocline/common.h"
