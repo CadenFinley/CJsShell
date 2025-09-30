a526c2003ada35c5f6b6c6692041e59985577624
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-09-29 20:24:52 -0500

    startup time bug fix

--- src/cjsh.cpp
+++ src/cjsh.cpp
@@ -337,7 +337,7 @@ static void start_interactive_process() {
     // Set the startup duration as the initial command duration for the
     // prompt
     if (g_shell && g_theme) {
-        g_shell->set_initial_duration(startup_duration.count() * 1000);
+        g_shell->set_initial_duration(startup_duration.count());
     }
 
     if (g_title_line) {
