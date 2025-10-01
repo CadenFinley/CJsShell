90a65adcb7c490ebed2f5449cabc7294dd34da51
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-09-30 21:42:17 -0500

    version forward

--- include/cjsh.h
+++ include/cjsh.h
@@ -18,7 +18,7 @@
 const bool PRE_RELEASE = false;
 const std::string pre_release_line = " (pre-release)";
 // using semver.org principles MAJOR.MINOR.PATCH
-const std::string c_version = "3.5.14" + (PRE_RELEASE ? pre_release_line : "");
+const std::string c_version = "3.5.15" + (PRE_RELEASE ? pre_release_line : "");
 
 #ifndef CJSH_GIT_HASH
 #define CJSH_GIT_HASH "unknown"
--- toolchain/nob/nob.c
+++ toolchain/nob/nob.c
@@ -3,7 +3,7 @@
 #include "nob.h"
 
 #define PROJECT_NAME "cjsh"
-#define VERSION "3.5.14"
+#define VERSION "3.5.15"
 
 #include <stdbool.h>
 #include <stdlib.h>
