06114e32a4d44bdff19c1f8fbbbba922a43134a8
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-10-02 22:15:12 -0500

    removed unneeded var

--- include/cjsh.h
+++ include/cjsh.h
@@ -10,9 +10,8 @@ class Ai;
 class Plugin;
 
 const bool PRE_RELEASE = true;
-const std::string pre_release_line = " (pre-release)";
 // using semver.org principles MAJOR.MINOR.PATCH
-const std::string c_version = "3.6.1" + (PRE_RELEASE ? pre_release_line : "");
+const std::string c_version = std::string("3.6.1") + (PRE_RELEASE ? " (pre-release)" : "");
 
 #ifndef CJSH_GIT_HASH
 #define CJSH_GIT_HASH "unknown"
