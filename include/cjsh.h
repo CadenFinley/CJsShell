4c61b04279e8010fc93fc9fb3c1353a8c25ac024
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-10-05 03:01:25 -0500

    version forward

--- include/cjsh.h
+++ include/cjsh.h
@@ -8,7 +8,7 @@ class Shell;
 class Theme;
 class Ai;
 
-const bool PRE_RELEASE = true;
+const bool PRE_RELEASE = false;
 // using semver.org principles MAJOR.MINOR.PATCH
 constexpr const char* c_version_base = "3.7.2";
 
--- toolchain/nob/nob.c
+++ toolchain/nob/nob.c
@@ -3,7 +3,7 @@
 #include "nob.h"
 
 #define PROJECT_NAME "cjsh"
-#define VERSION "3.7.1"
+#define VERSION "3.7.2"
 
 #include <stdbool.h>
 #include <stdlib.h>
