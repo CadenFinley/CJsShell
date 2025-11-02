c8cc2d56667a856c4266d0749026af33ff959a2e
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-11-01 20:39:06 -0500

    bump version

--- include/cjsh.h
+++ include/cjsh.h
@@ -7,7 +7,7 @@
 class Shell;
 
 const bool PRE_RELEASE = false;
-constexpr const char* c_version_base = "3.11.4";
+constexpr const char* c_version_base = "3.11.5";
 
 inline std::string get_version() {
     static std::string cached_version =
