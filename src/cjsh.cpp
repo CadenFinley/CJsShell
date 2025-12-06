9cb99ac806c24b9310a15cdb783617ec5eaa1fe3
CadenFinley <caden.finley0789@gmail.com>
2025-12-06 03:56:53 -0600

    version forward for release

--- src/cjsh.cpp
+++ src/cjsh.cpp
@@ -33,8 +33,8 @@
 #include "usage.h"
 #include "version_command.h"
 
-const bool PRE_RELEASE = true;
-const char* const c_version_base = "4.0.1";
+const bool PRE_RELEASE = false;
+const char* const c_version_base = "4.1.0";
 
 std::string get_version() {
     static std::string cached_version =
