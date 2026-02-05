6b1af50cac69bd92f53341a3c5a7833379250008
CadenFinley <caden.finley0789@gmail.com>
2026-02-05 11:41:23 -0600

    change resize

--- src/isocline/editline.c
+++ src/isocline/editline.c
@@ -2523,6 +2523,15 @@ edit_loop_entry:
                 }
             }
 
+            // update terminal in case of a resize (also detect polling changes)
+            bool should_resize = tty_term_resize_event(env->tty);
+            if (!should_resize && term_update_dim(env->term)) {
+                should_resize = true;
+            }
+            if (should_resize) {
+                edit_resize(env, &eb);
+            }
+
             // clear hint only after a potential resize (so resize row calculations
             // are correct)
             const bool had_hint = (sbuf_len(eb.hint) > 0);
