04a84d91959ac42fb282bbc13df69198c2a5f9ef
CadenFinley <caden.finley0789@gmail.com>
2026-02-05 01:13:11 -0600

    signal trigger for isocline

--- src/signal_handler.cpp
+++ src/signal_handler.cpp
@@ -40,6 +40,7 @@
 #include "cjsh.h"
 #include "error_out.h"
 #include "exec.h"
+#include "isocline.h"
 #include "job_control.h"
 #include "shell.h"
 #include "shell_env.h"
@@ -536,6 +537,7 @@ void SignalHandler::signal_handler(int signum) {
 #ifdef SIGWINCH
         case SIGWINCH: {
             s_sigwinch_received = 1;
+            ic_notify_resize();
 
             break;
         }
